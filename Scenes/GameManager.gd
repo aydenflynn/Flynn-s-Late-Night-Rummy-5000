a63832dc572334c7dfd57275fa0d82a8924c8dc4
aydenflynn <flynnayden@gmail.com>
2026-05-14 18:21:39 -0400

    Another layout test

--- Scenes/GameManager.gd
+++ Scenes/GameManager.gd
@@ -9,22 +9,22 @@ var is_round_over
 func _ready() -> void:
 	is_round_over = false
 	
-	var screen_size = get_viewport().size
-	print(screen_size)
-	
-	# layout setup here
-	$MeldButton.position.x = (screen_size.x / 2) - (screen_size.x / 8)
-	$MeldButton.position.y = (screen_size.y / 2) + (screen_size.y / 5)
-	
-	$DiscardButton.position.x = (screen_size.x / 2) + (screen_size.x / 8)
-	$DiscardButton.position.y = (screen_size.y / 2) + (screen_size.y / 5)
-	
-	$Deck.position.x = (screen_size.x / 2) - (screen_size.x / 10)
-	$Deck.position.y = (screen_size.y / 2) + (screen_size.y / 40)
-	
-	$Discard.position.x = (screen_size.x / 2) + (screen_size.x / 10)
-	$Discard.position.y = (screen_size.y / 2) + (screen_size.y / 40)
-	
+	#var screen_size = get_viewport().size
+	#print(screen_size)
+	#
+	## layout setup here
+	#$MeldButton.position.x = (screen_size.x / 2) - (screen_size.x / 8)
+	#$MeldButton.position.y = (screen_size.y / 2) + (screen_size.y / 5)
+	#
+	#$DiscardButton.position.x = (screen_size.x / 2) + (screen_size.x / 8)
+	#$DiscardButton.position.y = (screen_size.y / 2) + (screen_size.y / 5)
+	#
+	#$Deck.position.x = (screen_size.x / 2) - (screen_size.x / 10)
+	#$Deck.position.y = (screen_size.y / 2) + (screen_size.y / 40)
+	#
+	#$Discard.position.x = (screen_size.x / 2) + (screen_size.x / 10)
+	#$Discard.position.y = (screen_size.y / 2) + (screen_size.y / 40)
+	#
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
