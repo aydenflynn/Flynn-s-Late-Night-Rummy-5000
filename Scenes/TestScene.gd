31a76a9e4c4228e8c7d8702174559b53fd3d0cdd
aydenflynn <flynnayden@gmail.com>
2026-05-14 00:36:59 -0400

    Test pushing a change to site. Test window stretch mode

--- Scenes/TestScene.gd
+++ Scenes/TestScene.gd
@@ -1,22 +1,9 @@
 extends Node2D
 
-var card_values = ["a", "2", "3", "4", "5", "6", "7", "8", "9", "10", "j", "q", "k", "a"]
-
-var test_hand = ["5", "k", "a", "3", "q", "4"]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	# sort test_hand
-	var value_array = []
-	for card in test_hand:
-		value_array.append(card_values.find(card))
-	value_array.sort()
-	test_hand.clear()
-	
-	for card in value_array:
-		test_hand.append(card_values[card])
-	
-	print(test_hand)
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
